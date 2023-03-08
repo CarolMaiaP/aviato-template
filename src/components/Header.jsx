33d956c59abae8515e6bf85faceec75a8ad90b2a
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-03-08 20:15:57 -0300

    feat: header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { GiPhone } from 'react-icons/gi'
+import { SlBag } from 'react-icons/sl'
+
+export function Header(){
+  return(
+    <header>
+      <div className="contact">
+        <GiPhone size={24} /> 
+        <p>4191 4399</p>
+      </div>
+      <div className="logo">
+        <h3>Aviato</h3>
+      </div>
+      <div className="cart">
+        <SlBag size={24} />
+        <div className="quantity">
+          <p>(0) - </p>
+        </div>
+        <div className="price">
+          <p><strong>R$ 0.00</strong></p>
+        </div>
+      </div>
+    </header>
+  )
+}
