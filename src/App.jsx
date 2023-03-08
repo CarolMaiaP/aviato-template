3e4a8d0035dcb26effe0949c17596afcd888669b
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-03-08 20:15:27 -0300

    feat: Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Footer } from "./components/Footer"
+import { Header } from "./components/Header"
+import { Navbar } from "./components/Navbar"
+import { Category } from "./pages/Category"
+import { Home } from "./pages/Home"
+import { Product } from "./pages/Product"
+
 function App() {
   return (
     <div className="App">
-      <h1>Aviato Template</h1>
+      <Router>
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/categoria" element={<Category />}/>
+          <Route path="/produto" element={<Product />}/>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   )
 }
