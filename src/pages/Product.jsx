136b59f2cc126215e30213a3d4acd4927f2141aa
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-03-10 14:43:01 -0300

    feat: product page added

---
+++ src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import { BannerProduto } from "../components/Produto/BannerProduto";
+import { ProductDetails } from "../components/Produto/ProductDetails";
+
+export function Product(){
+  return(
+    <>
+      <BannerProduto />
+      <ProductDetails />
+    </>
+  )
+}
