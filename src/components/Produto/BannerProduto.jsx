d1fdc4cee0f190ec8b3f5bbe9ed51783fee3aefc
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-03-10 14:45:08 -0300

    feat: banner product component added

---
+++ src/components/Produto/BannerProduto.jsx
@@ -0,0 +1,14 @@
+import '../../styles/produto.scss'
+
+export function BannerProduto(){
+  return(
+    <>
+      <div className="banner">
+        <div className="text">
+          <p>Home - Blusas e Camisas - Gravida Est Quis Euismod</p>
+        </div>
+      </div>
+    </>
+  )
+  
+}
