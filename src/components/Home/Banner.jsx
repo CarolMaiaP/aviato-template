7dac0319fb5b51f9bff75aecd3cf6b9f7d869fa2
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-03-10 14:41:02 -0300

    feat: banner navigation added

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,7 +1,5 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
-
 import principalBanner from '../../assets/principal-banner.png'
 
 
@@ -10,17 +8,15 @@ import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 
 export default () => {
-
   return (
     <Swiper
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={1}
-      navigation
+      navigation={true}
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={(swiper) => swiper.slideNext()}
     >
       <SwiperSlide>
         <img src={principalBanner} alt="Banner principal" />
