0fc83563376292e2f1e7df08bbaed74a57957cb3
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-03-10 14:43:44 -0300

    feat: banner category component added

---
+++ src/components/Category/BannerCategory.jsx
@@ -0,0 +1,10 @@
+export function BannerCategory(){
+  return(
+    <div className="banner">
+      <div className="text">
+        <h3>Blusas</h3>
+        <p>Home - Blusas e Camisas - Blusas</p>
+      </div>
+    </div>
+  )
+}
